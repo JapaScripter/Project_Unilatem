01d5e2cbe1a5c222f6fe9ff079d4e1a765d818b5
JP <80179611+JapaScripter@users.noreply.github.com>
2024-05-22 11:46:12 -0300

    V.0.4

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*--|Mobile Menu|--*/
+function toggleMenu(){
+  var menuList = document.getElementById('lsm');
+  if (menuList.style.display ==='none' || menuList.style.display ===''){
+  menuList.style.display ='flex';}
+  else {
+    menuList.style.display ='none';
+  }
+}
+function toggleoffMenu(){
+  var menuList = document.getElementById('lsm');
+  var close = document.getElementById('cmn');
+  if (menuList.style.display ==='none' || menuList.style.display ===''){
+  menuList.style.display ='flex';}
+  else {
+    menuList.style.display ='none';
+  }
+}
+/*--|Mobile Menu|--*/
+
 /*!
  * Particleground
  *
