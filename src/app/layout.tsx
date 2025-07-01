911580d3f1729e3b5493c41ba835ac8f9ab2f8fa
JapaScripter <unilatem@gmail.com>
2025-07-01 15:02:54 -0300

    V.4.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: "Unique é único",
   description: "Unique é o site dos sites",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.ico",
   },
 };
 
